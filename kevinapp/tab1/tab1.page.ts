d7d458dab14686598afc3da825b63f70cd11a360
gohkevin <44095759+gohkevin@users.noreply.github.com>
2019-02-20 16:18:55 +0800

    Add files via upload

---
+++ kevinapp/tab1/tab1.page.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+//import firebase from 'firebase';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+    public isSearchBarOpened = false;
+
+    /*firestore = firebase.storage();
+    constructor(){}
+      getAllImage(){
+        return new Promise((resolve,reject)) => {
+          this.firestore.ref('/picmsgs').child(firebase.auth().currentUser.uid).getDownloadURL().then((gs://winey-b8c69.appspot.com/) => {
+            console.log(url)
+          })
+        }*/
+
+      }
+    
+
